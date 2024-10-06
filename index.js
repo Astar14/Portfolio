3c84678150fecee5c9bef99615cf5d65393d1a0a
Astar14 <tamit8587@gmail.com>
2024-10-06 14:10:45 +0530

    Created a Portfolio using Html/CSS

---
+++ index.js
@@ -0,0 +1,16 @@
+// let slider = document.getElementById('card-slider')
+// let projectCard = document.querySelectorAll('.project-card')
+// let currentIndex = 0;
+
+// const slideLeft = () => {
+//     if (currentIndex > 0) {
+//         currentIndex--;
+//     }
+// }
+
+// const slideRight = () => {
+//     if (currentIndex < projectCard.length-1) {
+//         currentIndex++;
+       
+//     }
+// }
